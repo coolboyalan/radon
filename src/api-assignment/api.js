77a00aa1dc2eefa25ca9dc83c3de1c4831083e19
coolboyalan <deepaky954000@gmail.com>
2022-06-03 00:03:47 +0530

    API PROBLEM

---
+++ src/api-assignment/api.js
@@ -0,0 +1,5 @@
+const movies = ["Movie1","Movie2","Movie3","Movie4"]
+
+module.exports={
+    movies:movies,
+}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,34 +1,16 @@
 const express = require('express');
-const myHelper = require('../util/helper')
+const problem = require('../api-assignment/api')
 const underscore = require('underscore')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+router.get('/movies', function (req, res) {  //PROBLEM1
+    res.send(JSON.stringify(problem.movies))
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
+router.get('/movies/:movieno', function (req, res) { //PROBLEM2 & PROBLEM3
+  res.send(problem.movies[req.params.movieno - 1] || "This isn't a valid index")
 });
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
 
 router.get('/candidates/:canidatesName', function(req, res){
     console.log('The request objects is '+ JSON.stringify(req.params))
