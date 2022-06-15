4ec9e2e4d72f19ff49a6186e002af2c1b21b8881
coolboyalan <deepaky954000@gmail.com>
2022-06-16 00:10:19 +0530

    15June-Authorization Assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,16 +3,17 @@ const userModel = require("../models/userModel");
 const mongoose = require("mongoose");
 const checkId = mongoose.isValidObjectId;
 
-const createUser = async function (req, res) { //PROBLEM1
+const createUser = async function (req, res) {
+  //PROBLEM1
   let data = req.body;
   let savedData = await userModel.create(data);
   res.send({ msg: savedData });
 };
 
-const loginUser = async function (req, res) { //PROBLEM2
+const loginUser = async function (req, res) {
+  //PROBLEM2
   let userName = req.body.emailId;
   let password = req.body.password;
-
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
     return res.send({
@@ -31,19 +32,8 @@ const loginUser = async function (req, res) { //PROBLEM2
   res.send({ status: true, token: token });
 };
 
-const getUserData = async function (req, res) { //PROBLEM3
-  // let token = req.headers["x-Auth-token"];
-  // if (!token) token = req.headers["x-auth-token"];
-  // if (!token) return res.send({ status: false, msg: "Token Missing" });
-
-  // let flow = false;
-  // jwt.verify(token, "deepakSingh", (err, payload) => {
-  //   if (err) {
-  //     flow = true;
-  //     return res.send({ msg:err.message });
-  //   }
-  // });
-  // if (flow) return;
+//PROBLEM3
+const getUserData = async function (req, res) {
   let userId = req.params.userId;
   if (!checkId(userId)) return res.send({ msg: "Please check user id" });
   let userDetails = await userModel.findById(userId);
@@ -52,53 +42,36 @@ const getUserData = async function (req, res) { //PROBLEM3
   res.send({ status: true, data: userDetails });
 };
 
-const updateUser = async function (req, res) { //PROBLEM4
-  // let token = req.headers["x-Auth-token"];
-  // if (!token) token = req.headers["x-auth-token"];
-  // if (!token) return res.send({ status: false, msg: "Token Missing" });
-
-  // let flow = false;
-  // jwt.verify(token, "deepakSingh", (err, payload) => {
-  //   if (err) {
-  //     flow = true;
-  //     return res.send({ msg:err.message });
-  //   }
-  // });
-  // if (flow) return;
+//PROBLEM4
+const updateUser = async function (req, res) {
   let userId = req.params.userId;
   if (!checkId(userId)) return res.send({ msg: "Please check user id" });
-  let userDetails = await userModel.findByIdAndUpdate(userId,req.body,{new:true});
+  let userDetails = await userModel.findByIdAndUpdate(userId, req.body, {
+    new: true,
+  });
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
   res.send({ status: true, data: userDetails });
 };
 
-const deleteUser = async function (req, res) { //PROBLEM5
-  // let token = req.headers["x-Auth-token"];
-  // if (!token) token = req.headers["x-auth-token"];
-  // if (!token) return res.send({ status: false, msg: "Token Missing" });
-
-  // let flow = false;
-  // jwt.verify(token, "deepakSingh", (err, payload) => {
-  //   if (err) {
-  //     flow = true;
-  //     return res.send({ msg:err.message });
-  //   }
-  // });
-  // if (flow) return;
+//PROBLEM5
+const deleteUser = async function (req, res) {
   let userId = req.params.userId;
   if (!checkId(userId)) return res.send({ msg: "Please check user id" });
-  let userDetails = await userModel.findByIdAndUpdate(userId,{isDeleted:true},{new:true});
+  let userDetails = await userModel.findByIdAndUpdate(
+    userId,
+    { isDeleted: true },
+    { new: true }
+  );
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
   res.send({ status: true, data: userDetails });
 };
-
 
 module.exports = {
-  createUser:createUser,
-  loginUser:loginUser,
-  getUserData:getUserData,
-  updateUser:updateUser,
-  deleteUser:deleteUser
-}
+  createUser: createUser,
+  loginUser: loginUser,
+  getUserData: getUserData,
+  updateUser: updateUser,
+  deleteUser: deleteUser,
+};
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,19 +1,19 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const auth = async function (req, res, next) {
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
   if (!token) return res.send({ status: false, msg: "Token Missing" });
 
-  let flow = false;
   jwt.verify(token, "deepakSingh", (err, payload) => {
     if (err) {
-      flow = true;
       return res.send({ msg: err.message });
     }
+    if (payload.userId != req.params.userId) {
+      return res.send({ msg: "You ain't authorized to perform this action" });
+    }
+    next();
   });
-  if (flow) return;
-  next()
 };
 
-module.exports.auth = auth
+module.exports.auth = auth;
