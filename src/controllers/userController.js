24f0731ea54861e75f2d3ea28fd28effce6391e2
coolboyalan <deepaky954000@gmail.com>
2022-06-16 21:22:09 +0530

    try-catch-16June

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,67 +5,109 @@ const checkId = mongoose.isValidObjectId;
 
 const createUser = async function (req, res) {
   //PROBLEM1
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  res.send({ msg: savedData });
+  try {
+    let data = req.body;
+    if (Object.keys(data).length == 0)
+      return res.status(400).send({ msg: "Please send the correct input" });
+    let savedData = await userModel.create(data);
+    res.status(201).send({ msg: savedData });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ msg: error.message });
+  }
 };
 
 const loginUser = async function (req, res) {
   //PROBLEM2
-  let userName = req.body.emailId;
-  let password = req.body.password;
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "Incorrect username/password",
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({
+      emailId: userName,
+      password: password,
     });
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "Radon",
-      organisation: "FunctionUp",
-    },
-    "deepakSingh"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+    if (!user)
+      return res.status(401).send({
+        status: false,
+        msg: "Incorrect username/password",
+      });
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "Radon",
+        organisation: "FunctionUp",
+      },
+      "deepakSingh"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(200).send({ status: true, token: token });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ msg: error.message });
+  }
 };
 
 //PROBLEM3
 const getUserData = async function (req, res) {
-  let userId = req.params.userId;
-  if (!checkId(userId)) return res.send({ msg: "Please check user id" });
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
-  res.send({ status: true, data: userDetails });
+  try {
+    let userId = req.params.userId;
+    if (!checkId(userId))
+      return res.status(401).send({ msg: "Please check user id" });
+    let userDetails = await userModel.findById(userId);
+    if (!userDetails)
+      return res
+        .status(401)
+        .send({ status: false, msg: "No such user exists" });
+    res.status(200).send({ status: true, data: userDetails });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ msg: error.message });
+  }
 };
 
 //PROBLEM4
 const updateUser = async function (req, res) {
-  let userId = req.params.userId;
-  if (!checkId(userId)) return res.send({ msg: "Please check user id" });
-  let userDetails = await userModel.findByIdAndUpdate(userId, req.body, {
-    new: true,
-  });
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
-  res.send({ status: true, data: userDetails });
+  try {
+    let data = req.body;
+    let userId = req.params.userId;
+    if (!checkId(userId))
+      return res.status(401).send({ msg: "Please check user id" });
+    if (Object.keys(data).length == 0)
+      return res.status(400).send({ msg: "Please send the correct input" });
+    let userDetails = await userModel.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    if (!userDetails)
+      return res
+        .stauts(401)
+        .send({ status: false, msg: "No such user exists" });
+    res.status(200).send({ status: true, data: userDetails });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ msg: error.message })
+  }
 };
 
 //PROBLEM5
 const deleteUser = async function (req, res) {
-  let userId = req.params.userId;
-  if (!checkId(userId)) return res.send({ msg: "Please check user id" });
-  let userDetails = await userModel.findByIdAndUpdate(
-    userId,
-    { isDeleted: true },
-    { new: true }
-  );
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
-  res.send({ status: true, data: userDetails });
+  try {
+    let userId = req.params.userId;
+    if (!checkId(userId))
+      return res.status(401).send({ msg: "Please check user id" });
+    let userDetails = await userModel.findByIdAndUpdate(
+      userId,
+      { isDeleted: true },
+      { new: true }
+    );
+    if (!userDetails)
+      return res
+        .status(401)
+        .send({ status: false, msg: "No such user exists" });
+    res.status(200).send({ status: true, data: userDetails });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ msg: error.message });
+  }
 };
 
 module.exports = {
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,19 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const auth = async function (req, res, next) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "Token Missing" });
-
-  jwt.verify(token, "deepakSingh", (err, payload) => {
-    if (err) {
-      return res.send({ msg: err.message });
-    }
-    if (payload.userId != req.params.userId) {
-      return res.send({ msg: "You ain't authorized to perform this action" });
-    }
-    next();
-  });
+  try {
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token)
+      return res.status(401).send({ status: false, msg: "Token Missing" });
+    
+    jwt.verify(token, "deepakSingh", (err, payload) => {
+      if (err) {
+        return res.status(401).send({ msg: err.message });
+      }
+      if (payload.userId != req.params.userId) {
+        return res
+          .status(403)
+          .send({ msg: "You ain't authorized to perform this action" });
+      }
+      next();
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ error: err.message });
+  }
 };
 
 module.exports.auth = auth;
